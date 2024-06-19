34c4dc8302bc1f3d05aee72d4c3ba4b5a1fdd06c
Rafael Magaña <raismaav.2510@gmail.com>
2024-06-18 18:16:32 -0600

    Test interface

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import cv2
 import pytesseract
+import tkinter as tk
+from tkinter import messagebox
 
 image = cv2.imread('test_image.png')
-cv2.imshow('Image', image)
-cv2.waitKey(0)
+# cv2.imshow('Image', image)
+# cv2.waitKey(0)
 
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 text = pytesseract.image_to_string(image)
 print(text)
 
+def check_input():
+    input_text = entry.get()
+    if input_text in text:
+        messagebox.showinfo("Resultado", "Numero de factura correcto")
+    else:
+        messagebox.showinfo("Resultado", "Numero de factura incorrecto")
+
+root = tk.Tk()
+root.title("Verificador de factura")
+
+entry = tk.Entry(root)
+entry.pack()
+
+button = tk.Button(root, text="Verificar", command=check_input)
+button.pack()
+
+root.mainloop()
