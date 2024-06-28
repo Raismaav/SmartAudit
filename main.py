4036c63c8c5b3bd04deb2c20a0f21241be35cf7c
Rafael Magaña <raismaav.2510@gmail.com>
2024-06-28 17:16:05 -0600

    multiplatform support

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import cv2
 import pytesseract
 import tkinter as tk
 from tkinter import messagebox
+import platform
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+if platform.system() == 'Windows':
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 image = cv2.imread('test_image.png')
 # cv2.imshow('Image', image)
