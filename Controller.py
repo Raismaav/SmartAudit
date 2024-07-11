6cf0a1c3bc2143191e3a90c97e5f14c792e237ed
Rafael Magaña <raismaav.2510@gmail.com>
2024-07-11 13:52:49 -0600

    OCR.py MVC implemented

--- Controller.py
+++ Controller.py
@@ -1,10 +1,22 @@
 from PDFReader import PDFReader
+from OCR import OCR
 
+archivo = 'test_image.png'
 # Reemplaza 'ruta/a/tu/archivo.pdf' con la ruta real a tu archivo PDF
-pdf_reader = PDFReader('test.pdf')
+pdf_reader = PDFReader(archivo)
 texto = pdf_reader.read_pdf()
 if pdf_reader.is_readable():
     print("El PDF es legible. Aquí está el texto:")
     print(texto)
 else:
-    print("El PDF no es legible.")
+    # Crea una instancia de OCR pasando la ruta del archivo
+    lector_ocr = OCR(archivo)
+
+    # Llama al método read_text para extraer el texto del archivo
+    texto = lector_ocr.read_text()
+
+    if lector_ocr.is_readable():
+        print("El archivo es legible. Aquí está el texto extraído:")
+        print(texto)
+    else:
+        print("El archivo no es legible o no se pudo extraer texto.")
---
+++ OCR.py
@@ -0,0 +1,38 @@
+from pdf2image import convert_from_path
+import pytesseract
+import os
+
+class OCR:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.text = None
+        self.images = None
+        # Verifica si el archivo es un PDF
+        if self.file_path.endswith('.pdf'):
+            self.images = self.pdf_to_img(self.file_path)
+        else:
+            # Si no es un PDF, asume que es una imagen y la almacena en una lista
+            self.images = [self.file_path]
+
+    def pdf_to_img(self, pdf_file):
+        """Convierte un archivo PDF a imágenes."""
+        return convert_from_path(pdf_file)
+
+    def ocr_core(self, images):
+        """Realiza OCR en las imágenes y extrae el texto."""
+        text = ""
+        for img in images:
+            text += pytesseract.image_to_string(img)
+        return text
+
+    def read_text(self):
+        """Lee el texto del archivo PDF o imagen."""
+        if not self.images:
+            print("No hay imágenes para procesar.")
+            return None
+        self.text = self.ocr_core(self.images)
+        return self.text
+
+    def is_readable(self):
+        """Verifica si fue posible extraer texto."""
+        return bool(self.text)
